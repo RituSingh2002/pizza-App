68fff5036478f458b46bda005da3ed9e726fe3a8
RituSingh2002 <ritusingh29g@gmail.com>
2022-01-30 06:58:27 +0530

    Add login and register page

--- server.js
+++ server.js
@@ -6,16 +6,26 @@ const expressLayout=require('express-ejs-layouts');
 const path=require('path');
 // assets
 app.use(express.static('public'));
-app.get('/',function(req,res){
-    // res.send("Hello from server");
-    // for page
-    res.render('home')
-})
+ 
 
 // Set template engine
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+app.get('/',function(req,res){
+    // res.send("Hello from server");
+    // for page
+    res.render('home')
+})
+app.get('/cart',(req,res)=>{
+    res.render('./customers/cart');
+})
+app.get('/login',(req,res)=>{
+    res.render('./auth/login');
+})
+app.get('/register',(req,res)=>{
+    res.render('./auth/register');
+})
 app.listen(port,function(){
     console.log(`Listening on port 3000 ${port}`)
 })
